4b3022b78e1d7bd70f72097f141b0bb0b2a3fdc8
Tendry24 <hei.tendry.2@gmail.com>
2024-05-26 21:22:17 +0300

    Initial commit

---
+++ src/main/java/org/kfc/kfcmada/KfCmadaApplication.java
@@ -0,0 +1,13 @@
+package org.kfc.kfcmada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfCmadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KfCmadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kfc/kfcmada/KfCmadaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kfc.kfcmada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfCmadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
